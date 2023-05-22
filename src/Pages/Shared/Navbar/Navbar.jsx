85cd351a167123a24bbc2a0b2b5b23f351d86b8b
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-05-22 23:10:03 +0600

    carosal banner added

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
 
 
     return (
-        <div className="navbar bg-base-100">
+        <div className="fixed z-10 bg-opacity-30 text-white bg-black max-w-screen-xl navbar">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
