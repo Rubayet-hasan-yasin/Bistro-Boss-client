6ca526721485edb39b3638501fcd86d2d396ddfa
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-05-23 20:29:08 +0600

    category added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../Banner/Banner";
+import Category from "../Category/Category";
 
 const Home = () => {
     return (
         <div>
             <Banner/>
+            <Category/>
             <h2>this is home</h2>
         </div>
     );
