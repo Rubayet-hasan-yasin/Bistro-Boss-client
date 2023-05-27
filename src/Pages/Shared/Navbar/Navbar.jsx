834c25afdd1e01c77b5ff0e3679885035b9ebef3
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-05-27 11:06:40 +0600

    firebase added

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ const Navbar = () => {
         <li><Link to={'/'}>Home</Link></li>
         <li><Link to={'/menu'}>Our menu</Link></li>
         <li><Link to={'/order/salad'}>OUR SHOP</Link></li>
+        <li><Link to={'/login'}>Login</Link></li>
     </>
 
 
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,9 @@ import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
 import Menu from "../Pages/Menu/Menu/Menu";
 import Order from "../Pages/Order/Order/Order";
+import Login from "../Pages/Login/Login";
+
+
 
 
 export const router = createBrowserRouter([
@@ -21,7 +24,12 @@ export const router = createBrowserRouter([
             {
                 path: 'order/:category',
                 element: <Order/>
+            },
+            {
+                path: '/login',
+                element: <Login/>
             }
+            
         ]
     }
 ])
