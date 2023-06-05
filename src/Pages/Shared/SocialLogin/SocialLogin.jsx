ccefc3d92ef48bd1a112e417b36385598cf538bc
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2023-06-05 14:55:26 +0600

    deploy

--- src/Pages/Shared/SocialLogin/SocialLogin.jsx
+++ src/Pages/Shared/SocialLogin/SocialLogin.jsx
@@ -20,7 +20,7 @@ const SocialLogin = () => {
 
                 const saveUser = { name: loggedInUser.displayName, email: loggedInUser.email };
 
-                fetch(`http://localhost:5000/users`, {
+                fetch(`https://bistro-boss-server-alpha.vercel.app/users`, {
                     method: "POST",
                     headers: {
                         'content-type': 'application/json'
